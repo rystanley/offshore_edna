9848c62c1ec637d0c67fd8a436e91fe60d45ec81
Stanley <Ryan.Stanley@dfo-mpo.gc.ca>
2023-04-26 16:33:08 -0300

    updated to bug fix the issue with the iNext updates

--- code/iNEXT (2020 samples).R
+++ code/iNEXT (2020 samples).R
@@ -102,8 +102,8 @@
     p2_solo <- ggplot()+
               geom_ribbon(data=plotdata%>%filter(denominator == "Number of sampling sites"),aes(x=x,ymin=y.lwr,ymax=y.upr,fill=var),alpha=0.5)+
               geom_line(data=plotdata%>%filter(denominator == "Number of sampling sites"),aes(x=x,y=y,col=var),lty=2,lwd=1.25)+
-              geom_line(data=filter(plotdata,method!="extrapolated",denominator == "Number of sampling sites"),aes(x=x,y=y,col=var),lwd=1.25)+
-              geom_point(data=filter(plotdata,method=="observed",denominator == "Number of sampling sites"),aes(x=x,y=y,fill=var),shape=21,size=4)+ #change in type to make it clearer.
+              geom_line(data=filter(plotdata,method!="Extrapolation",denominator == "Number of sampling sites"),aes(x=x,y=y,col=var),lwd=1.25)+
+              geom_point(data=filter(plotdata,method=="Observed",denominator == "Number of sampling sites"),aes(x=x,y=y,fill=var),shape=21,size=4)+ #change in type to make it clearer.
               theme_bw()+
               facet_grid(type2~.,scales="free")+
               theme(strip.background = element_rect(, colour = "black", fill = "white"),
@@ -115,7 +115,7 @@
               scale_y_continuous(expand=c(0,0.02))+
               scale_fill_manual(values=plotcols)+ # to increase contrast
               scale_colour_manual(values=plotcols)+
-              labs(x="Number of sampling sites",y="Species Richness",fill="",col="")
+              labs(x="Number of sampling sites",y="Taxon Richness",fill="",col="")
     
     ggsave("output/richness_sites.png",p2_solo,width=9,height=9,units="in",dpi=600)
     
